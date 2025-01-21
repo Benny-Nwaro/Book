9f552f9b0889780c2b77c8e0e24add6e569f124e
AROH EBENEZER <baroh3767@aol.com>
2025-01-21 11:38:30 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Collection prose = new Collection();
 
-        Book YellowSUn = new Book("Half of a yellow sun",
+        Book YellowSun = new Book("Half of a yellow sun",
                 "Chimamanda Adiche",
                 342567865 );
         Book MonteCristo = new Book("The count  of monte Cristo",
                 "Alexandre Dumas",
                 422569065 );
 
-        prose.addToCollection(YellowSUn);
+        prose.addToCollection(YellowSun);
         System.out.println(prose.getBookByTitle("half of a yellow sun").getAuthor());
     }
 }
